549badb99b8949c318abad0c63684c60c07ddc00
Italicz <james.angier@griffithuni.edu.au>
2020-08-07 16:06:59 +1000

    added maze grid and playing screen

--- app_class.py
+++ app_class.py
@@ -5,33 +5,89 @@ from settings import *
 pygame.init()
 vec = pygame.math.Vector2
 
+######################## SETUP ########################
 
 class App:
     def __init__(self):
         self.screen = pygame.display.set_mode((width, height))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.state = 'intro'
+        self.state = 'start'
+        self.cell_width =maze_width//28
+        self.cell_height = maze_height//30
+
+        self.load()
         #self.run()
     
     def run(self):
         while self.running:
-            if self.state == 'intro':
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
             self.clock.tick(fps)
         pygame.quit()
         sys.exit()
 
-    def intro_events(self):
+######################## OTHER FUNCTIONS ########################
+    
+    def draw_text(self, screen, pos, size, colour, font_name, text, centred=False):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(text, False, colour)
+        text_size = text.get_size()
+        if centred:
+            pos[0] = pos[0]-text_size[0]//2
+            pos[1] = pos[1]-text_size[1]//2
+        screen.blit(text, pos)
+
+    def load(self):
+        self.maze = pygame.image.load('images/maze.png')
+        self.maze = pygame.transform.scale(self.maze, (maze_width, maze_height))
+
+######################## START SCREEN ########################
+
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'    
 
-    def intro_update(self):
+    def start_update(self):
         pass
 
-    def intro_draw(self):
+    def start_draw(self):
+        self.screen.fill(black)
+        self.draw_text(self.screen, [width//2, height//2], start_text_size, (170, 140, 60), start_font, 'PUSH SPACE TO START', centred=True)
+        self.draw_text(self.screen, [width//2, height//2+100], start_text_size, (0, 140, 60), start_font, 'GOOD LUCK!', centred=True)
+        self.draw_text(self.screen, [0,0], start_text_size, (255, 255, 255), start_font, 'HIGH SCORE', centred=False)
+
         pygame.display.update()
-               
+
+######################## PLAYING SCREEN ########################
+
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    
+    def playing_update(self):
+        pass
+    
+    def playing_draw(self):
+        self.screen.fill(black)
+        self.screen.blit(self.maze, (top_bottom_buffer//2, top_bottom_buffer//2))
+        self.draw_grid()
+        pygame.display.update()
+
+    def draw_grid(self):
+        for x in range(width//self.cell_width):
+            pygame.draw.line(self.maze, grey, (x*self.cell_width, 0), (x*self.cell_width, height))
+        for x in range(height//self.cell_height):
+            pygame.draw.line(self.maze, grey, (0, x*self.cell_height), (width, x*self.cell_height))
--- settings.py
+++ settings.py
@@ -1,8 +1,15 @@
 #Screen settings
-width, height = 500, 700
+width, height = 610, 670
 fps = 60
+start_text_size = 30
+start_font = 'lithograph'
+top_bottom_buffer = 50
+maze_width, maze_height = width-top_bottom_buffer, height-top_bottom_buffer
 
 #Colour settings
+black = (0,0,0)
+red = (208, 22, 22)
+grey = (107,107,107)
 
 #Player settings
 
