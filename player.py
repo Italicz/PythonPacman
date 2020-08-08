dde081409a2b0d3c8faef50a107ea6c55abedfcb
Italicz <james.angier@griffithuni.edu.au>
2020-08-08 13:35:40 +1000

    added basic player movement

--- player.py
+++ player.py
@@ -7,10 +7,17 @@ class Player:
         self.app = app
         self.grid_pos = pos
         self.pixel_pos = vec((self.grid_pos.x*self.app.cell_width)+top_bottom_buffer//2+self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height)+top_bottom_buffer//2+self.app.cell_height//2)
-        print(self.grid_pos, self.pixel_pos)
+        self.direction = vec(1,0)
     
     def update(self):
-        pass
+        self.pixel_pos += self.direction
+
+        #Setting the grid positition to the same as the pixel position
+        self.grid_pos[0] = (self.pixel_pos[0]-top_bottom_buffer+self.app.cell_width//2)//self.app.cell_width+1
+        self.grid_pos[1] = (self.pixel_pos[1]-top_bottom_buffer+self.app.cell_height//2)//self.app.cell_height+1
 
     def draw(self):
         pygame.draw.circle(self.app.screen, player_colour, (int(self.pixel_pos.x), int(self.pixel_pos.y)), self.app.cell_width//2)
+
+    def move(self, direction):
+        self.direction = direction
